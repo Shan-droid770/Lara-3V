3080484542bd2f4bd173db1a2ab04fd51260f2e3
Shan-droid770 <kumaradissanayaka30@gmail.com>
2026-01-17 22:04:18 +0530

    Update session and configuration settings

--- settings.js
+++ settings.js
@@ -5,14 +5,14 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "3N5HBITT#Uq8el8Q8DbscEuqOjj01plTIlGKnGoKwzd7O8fpdOec", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "iJQH3bbR#M50r-5YlEqKQ9fYxfMqaXgnYX7d2s437HDm958LkwPs", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 MODE: process.env.MODE || "private",  //public | private | inbox | group
-OWNER_NUMBER: process.env.OWNER_NUMBER || "94763952882",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "94724898578",
 DEV: process.env.DEV || "94779062397",
 LANG: process.env.LANG || "EN", // EN | SI
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false",
+AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "false",
 AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
 STATUS_REPLY_MSG: process.env.STATUS_REPLY_MSG || "*_`ᴀᴜᴛᴏ ꜱᴛᴀᴛᴜꜱ ᴊᴜꜱᴛ ɴᴏᴡ ꜱᴇᴇɴ ʙʏ ʟᴀʀᴀ ᴍᴅ`_* 💃🏻",
 AUTO_BIO: process.env.AUTO_BIO || "false",
@@ -33,11 +33,11 @@ READ_MESSAGE: process.env.READ_MESSAGE || "false",
 READ_CMD: process.env.READ_CMD || "true",
 ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true",
 ANTI_VV: process.env.ANTI_VV || "true",
-ANTI_DELETE: process.env.ANTI_DELETE || "true",
-ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "same",  // inbox | same
+ANTI_DELETE: process.env.ANTI_DELETE || "false",
+ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "inbox",  // inbox | same
 WELCOME: process.env.WELCOME || "false",
 GOODBYE: process.env.GOODBYE || "false",
-ADMIN_EVENTS: process.env.ADMIN_EVENTS || "true",
+ADMIN_EVENTS: process.env.ADMIN_EVENTS || "false",
 INBOX_BLOCK: process.env.INBOX_BLOCK || "false",
 OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
 };
